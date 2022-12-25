58e6afdd0c3397842deb4c0f7790edc4239d6559
stewones <oi@stewan.io>
2022-12-25 15:02:45 -0300

    🔧

--- docs/mongo.md
+++ docs/mongo.md
@@ -1,4 +1,5 @@
 # start mac m1 process
 
 mongod --config /opt/homebrew/etc/mongod.conf --fork
+
 https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-os-x/
--- packages/server/src/lib/Cache.ts
+++ packages/server/src/lib/Cache.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright Intenseloop LTD All Rights Reserved.
+ * Copyright Elegante All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://elegante.dev/license
