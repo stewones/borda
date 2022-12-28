ec13a059d8fac2227c3ceae611dbdca719bb5a3a
stewones <oi@stewan.io>
2022-12-28 03:33:29 -0300

    fix: improve guid util

--- packages/sdk/src/lib/utils/guid.ts
+++ packages/sdk/src/lib/utils/guid.ts
@@ -1,17 +1,40 @@
-export function guid(size = -1) {
-  const s4 = () => {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  };
+import { isServer } from './isServer';
 
-  return size === 1
-    ? s4()
-    : size === 2
-    ? s4() + s4()
-    : size === 3
-    ? s4() + s4() + s4()
-    : size === 4
-    ? s4() + s4() + s4() + s4()
-    : s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4();
+export function guid(size = -1) {
+  return isServer() || process.env['NODE_ENV'] === 'test'
+    ? generateForServer(size)
+    : generateForClient(size);
+}
+
+function generateForClient(size: number) {
+  const uid = crypto.randomUUID();
+  return size > 0 && size < 5 ? uid.split('-').slice(0, size).join('') : uid;
+}
+
+function generateForServer(size: number) {
+  const uid = randomUUID();
+  return size > 0 && size < 5 ? uid.split('-').slice(0, size).join('') : uid;
+}
+
+function random() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+}
+
+function randomUUID() {
+  return (
+    random() +
+    random() +
+    '-' +
+    random() +
+    '-' +
+    random() +
+    '-' +
+    random() +
+    '-' +
+    random() +
+    random() +
+    random()
+  );
 }
