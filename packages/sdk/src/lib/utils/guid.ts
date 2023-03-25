232feeebf68276ae0ab6f0c13632e3cb156bcb10
stewones <oi@stewan.io>
2023-03-25 15:03:04 -0300

    fix: we can't ship `guid` using native api as it's breaking vite production builds.

--- packages/sdk/src/lib/utils/guid.ts
+++ packages/sdk/src/lib/utils/guid.ts
@@ -6,43 +6,31 @@
  * found in the LICENSE file at https://elegante.dev/license
  */
 
-import { isServer } from './isServer';
-
 export function guid(size = -1) {
-  return isServer() || process.env['NODE_ENV'] === 'test'
-    ? generateForServer(size)
-    : generateForClient(size);
-}
-
-function generateForClient(size: number) {
-  const uid = crypto.randomUUID();
-  return size > 0 && size < 5 ? uid.split('-').slice(0, size).join('') : uid;
-}
-
-function generateForServer(size: number) {
-  const uid = randomUUID();
-  return size > 0 && size < 5 ? uid.split('-').slice(0, size).join('') : uid;
+  return size === 1
+    ? random()
+    : size === 2
+    ? random() + random()
+    : size === 3
+    ? random() + random() + random()
+    : size === 4
+    ? random() + random() + random() + random()
+    : random() +
+      random() +
+      '-' +
+      random() +
+      '-' +
+      random() +
+      '-' +
+      random() +
+      '-' +
+      random() +
+      random() +
+      random();
 }
 
 function random() {
   return Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
     .substring(1);
-}
-
-function randomUUID() {
-  return (
-    random() +
-    random() +
-    '-' +
-    random() +
-    '-' +
-    random() +
-    '-' +
-    random() +
-    '-' +
-    random() +
-    random() +
-    random()
-  );
 }
